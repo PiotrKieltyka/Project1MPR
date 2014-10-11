834853ec0cba2230fbd92ca9212d408c10be54bf
Piotr <Piotr@192.168.1.100>
2014-10-11 11:21:45 +0200

    Second Release

--- MPRProject1/src/domain/Person.java
+++ MPRProject1/src/domain/Person.java
@@ -5,10 +5,54 @@ import java.util.*;
 public class Person {
 
 	private String firstName;
-		private String surName;
-		private String pesel;
-		private String idNumber;
-		private String birthDate;
-		private User user;
-		private List<Address> addresses;
-	}
+	private String surName;
+	private String pesel;
+	private String idNumber;
+	private String birthDate;
+	private User user;
+	private List<Address> addresses;
+
+public String getFirstName() {
+return firstName;
+}
+public void setFirstName(String firstName) {
+this.firstName = firstName;
+}
+public String getSurName() {
+return surName;
+}
+public void setSurName(String surName) {
+this.surName = surName;
+}
+public String getPesel() {
+return pesel;
+}
+public void setPesel(String pesel) {
+this.pesel = pesel;
+}
+public String getIdNumber() {
+return idNumber;
+}
+public void setIdNumber(String idNumber) {
+this.idNumber = idNumber;
+}
+public String getBirthDate() {
+return birthDate;
+}
+public void setBirthDate(String birthDate) {
+this.birthDate = birthDate;
+}
+public User getUser() {
+return user;
+}
+public void setUser(User user) {
+this.user = user;
+}
+public List<Address> getAddresses() {
+return addresses;
+}
+public void setAddresses(List<Address> addresses) {
+this.addresses = addresses;
+}
+
+}
--- MPRProject1/src/domain/User.java
+++ MPRProject1/src/domain/User.java
@@ -5,4 +5,24 @@ public class User {
 	private String login;
 	private String password;
 	private Person person;
+
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
 }
